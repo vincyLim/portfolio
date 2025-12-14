f3a266cbaea6290eab3b28551e4ecc0a5e3aab8f
vincyLim <lim.vincy188@1utar.my>
2025-12-14 14:23:44 +0800

    Wrong image url name

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: '/opengraph-image.png',
+        url: '/opengraph-image.jpg',
         width: 1200,
         height: 630,
         alt: 'Vincy Lim - Software Engineer Portfolio',
